b8adb360ad9556dd2334fe6706bcc646f5ee99cf
hesham <do.everything.hgh@gmail.com>
2026-04-19 23:35:25 +0200

    Auto-build (Minified): 2026-04-19T21:35:24.951Z

--- pages/merchant-portfolio/js/send-request-state.js
+++ pages/merchant-portfolio/js/send-request-state.js
@@ -1,11 +1,1 @@
-/**
- * @file pages/merchant-portfolio/js/send-request-state.js
- * @description Centralized state management for the Pharmacy Request page.
- */
-
-window.RequestState = {
-    merchant: null,
-    attachedImages: [],
-    isRecording: false,
-    maxImages: 4
-};
+window.RequestState={merchant:null,attachedImages:[],isRecording:!1,maxImages:4};
